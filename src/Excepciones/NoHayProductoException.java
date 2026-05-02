7e857cf4ab2886285175bd071ba79f6a1e70e493
Fefaarriagadav <jarriagada2025@udec.cl>
2026-05-02 17:44:58 -0400

    Enhance NoHayProductoException with comments
    Added a comment to describe the exception and its constructor.

--- src/Excepciones/NoHayProductoException.java
+++ src/Excepciones/NoHayProductoException.java
@@ -1,6 +1,13 @@
 package src.Excepciones;
+/**
+* excepcion que se activa si no queda producto en el deposito
+*/
 
 public class NoHayProductoException extends Exception {
+  /** constructor para definir el mensaje de error
+  * @param mensaje que describe el error
+  * @throws cuando se instancia NoHayProductoException
+  */
   public NoHayProductoException(String mensaje) {
     super(mensaje);
   }
