83222b0dbe55f6b42b4d36276de8175cb5129b38
Fefaarriagadav <jarriagada2025@udec.cl>
2026-05-02 17:49:45 -0400

    Add documentation to PagoInsuficienteException
    Added documentation comments for the exception class.

--- src/Excepciones/PagoInsuficienteException.java
+++ src/Excepciones/PagoInsuficienteException.java
@@ -1,6 +1,13 @@
 package src.Excepciones;
+/**
+* excepcion se activa cuando el pago es insufieciente m < al valor del producto seleccionado
+*/
 
 public class PagoInsuficienteException extends Exception {
+    /** constructor para definir el mensaje de error
+  * @param mensaje que describe el error
+  * @throws cuando se instancia PagoInsuficienteException
+  */
   public PagoInsuficienteException(String mensaje) {
     super(mensaje);
   }
