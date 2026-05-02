64de6fc3efb4940685c75a32e1100d4c04de47b1
Fefaarriagadav <jarriagada2025@udec.cl>
2026-05-02 17:46:49 -0400

    Enhance PagoIncorrectoException with Javadoc comments
    Added documentation comments for the exception class and its constructor.

--- src/Excepciones/PagoIncorrectoException.java
+++ src/Excepciones/PagoIncorrectoException.java
@@ -1,7 +1,14 @@
 package src.Excepciones;
+/**
+* excepcion que se activa si el pago (moneda) es insuficiente < al valor del producto
+*/
 
 public class PagoIncorrectoException extends Exception {
   public PagoIncorrectoException(String mensaje) {
+      /** constructor para definir el mensaje de error
+  * @param mensaje que describe el error
+  * @throws cuando se instancia PagoIncorrectoException
+  */
     super(mensaje);
   }
 }
