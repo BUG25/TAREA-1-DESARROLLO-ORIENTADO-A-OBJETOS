3b482fce962391a3ed5e82b16768c9c168e7c15b
Trini <chiquitita.Pilar@gmail.com>
2026-05-02 00:25:34 -0400

    Actualizacion de enum con IDs y nombres

--- Selección.java
+++ Selección.java
@@ -1,17 +1,27 @@
 pubic enum Seleccion {
   // definimos los precios respectivos a los precios
-COCA_COLA (1000), //precios cambiables
-SPRITE (900),
-FANTA (900),
-SNICKERS (600),
-SUPER8 (300);
+COCA_COLA (1000, 101, "CocaCola"), //precios cambiables
+SPRITE (900, 102, "Sprite"),
+FANTA (900, 201, "Fanta"),
+SNICKERS (600, 202, "Snickers"),
+SUPER8 (300, 203,"Super8");
 
 private final int precio;
-Seleccion (int precio) {
+private final int ID;
+private String nombre;
+Seleccion (int precio, int ID, String nombre) {
   this.precio = precio; // el cosntructor de la enumeración asigna los precios
+  this.ID = ID;
+  this.nombre = nombre;
 }
 
 public int getPrecio() {
   return precio;
+  }
+public int getID() {
+  return ID;
+}
+public String getNombre() {
+    return nombre;
   }
 }
