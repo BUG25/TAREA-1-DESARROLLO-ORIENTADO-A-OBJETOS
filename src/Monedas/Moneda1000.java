ae984939ba8b85f5345e03bf111d02d1617b1249
Fefaarriagadav <jarriagada2025@udec.cl>
2026-05-02 19:07:24 -0400

    Update Moneda1000.java

--- src/Monedas/Moneda1000.java
+++ src/Monedas/Moneda1000.java
@@ -1,6 +1,9 @@
 package src.Monedas;
 
 public class Moneda1000 extends Moneda{
+    /** constructor de la clase Moneda100
+    * llama al constructor de la clase padre (Moneda) y le asigan el valor fijo de 1000 
+    */
     public Moneda1000(){
         super (1000);
     }
