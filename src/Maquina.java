173b627deee469ce1ed319a6a59e62133347abe3
Fefaarriagadav <jarriagada2025@udec.cl>
2026-05-02 10:35:23 -0400

    Add Maquina.java with package and imports

---
+++ src/Maquina.java
@@ -0,0 +1,7 @@
+package src;
+import src.Depositos.Deposito;
+import src.Excepciones.*; // * es para traer todas las clases publicas dentro de la carpeta (PagoInsufiente, NoHayProducto, PagoIncorrecto)
+import src.Monedas.*; // * impotarar Moneda100, Moneda1000 y Moneda500
+import src.Productos.*; // todos los productos
+import src.Productos.Bebidas.*; // COCA_COLA, SPRITE,FANTA
+import src.Productos.Dulces.*; //SUPER8 Y SNICKERS
